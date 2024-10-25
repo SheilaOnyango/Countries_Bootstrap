24e65811ed29e076956bc6b06aa2f1f070fbe3a7
sheilaonyango <edusheila31@gmail.com>
2024-10-25 17:00:58 +0300

    Add envexample

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App.jsx";
-import { store } from "./store/store.js";
+import App from "./App";
+import { store } from "./store/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
